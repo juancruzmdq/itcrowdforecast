c33ec8ade1748287cacc73e4d07179b94957dcd2
Juan Cruz Ghigliani <juancruzmdq@gmail.com>
2018-05-05 09:40:14 -0300

    Project Begin

---
+++ itcrowdforecast/Views/CityListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CityListViewController.swift
+//  itcrowdforecast
+//
+//  Created by Juan Cruz Ghigliani on 5/5/18.
+//
+
+import Foundation
---
+++ itcrowdforecast/Views/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  itcrowdforecast
+//
+//  Created by Juan Cruz Ghigliani on 4/5/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
